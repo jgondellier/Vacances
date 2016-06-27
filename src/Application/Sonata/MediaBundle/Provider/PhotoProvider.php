b5e3586dcb926df3feaac9a2aada81aa45d287ad
jgondellier <jgondellier@yahoo.fr>
2016-06-27 23:23:38 +0200

    init

--- src/Application/Sonata/MediaBundle/Provider/PhotoProvider.php
+++ src/Application/Sonata/MediaBundle/Provider/PhotoProvider.php
@@ -1,11 +1,8 @@
 <?php
-namespace Sonata\MediaBundle\Provider;
+namespace Application\Sonata\MediaBundle\Provider;
 
-use Sonata\MediaBundle\Provider\BaseProvider;
-use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\MediaBundle\Model\MediaInterface;
-use Symfony\Component\Form\Form;
-use Sonata\MediaBundle\Provider\MediaProviderInterface;
+use Sonata\MediaBundle\Provider\ImageProvider;
 
 class PhotoProvider extends ImageProvider
 {
